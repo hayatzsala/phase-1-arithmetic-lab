5787358dda3748c33ee4256dd7b85c6f8ac7400b
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-08-10 03:00:51 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+var num1 = 2;
+var num2 = 31;
+var multiply = num1 * num2;
+
+var random  = Math.floor(Math.random() * 10) + 1;
+
+var num3 = 19;
+var num4 = 5;
+var mod = num3 % num4;
+
+var max = Math.max(1,2,20);
